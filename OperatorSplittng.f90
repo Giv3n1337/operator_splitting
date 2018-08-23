a0f64c507637ba6337425bba4bee13a8d3d729aa
David Kube <d.kube@gwaihir.physik.tu-berlin.de>
2018-08-23 17:55:59 +0200

    im here

---
+++ OperatorSplittng.f90
@@ -0,0 +1,24 @@
+program OperatorSplitting
+
+	implicit none
+	
+	! Simple Example: Operator Splitting   	
+
+
+	!<-----------------------  parameters  ----------------------------
+	! dimensions
+	integer :: ndims = 1
+	
+	! amount of cells
+	integer :: ncells = 1000
+
+	! space: interval from 0 to xmax
+	double precision :: xmax = 1.0
+
+	! mesh grid
+	integer, dimension(:,:,:), allocatable :: grid
+	
+	! 
+	  
+
+end program
